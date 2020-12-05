c90366eeb656f804df26109f7f096afe3103e3db
evgsavelov <savelov08@ya.ru>
2020-12-05 22:03:40 +0300

    complete day6 tasks

--- src/main/java/day6/Task1.java
+++ src/main/java/day6/Task1.java
@@ -2,6 +2,12 @@ package day6;
 
 public class Task1 {
     public static void main(String[] args) {
+        Car car1 = new Car();
+        car1.info();
+        car1.yearDifference(5);
 
+        Motorbike moto1 = new Motorbike(2021, "red", "sherco" );
+        moto1.info();
+        moto1.yearDifference(10);
     }
 }
--- src/main/java/day6/Task2.java
+++ src/main/java/day6/Task2.java
@@ -2,6 +2,13 @@ package day6;
 
 public class Task2 {
     public static void main(String[] args) {
+        Airplane air1 = new Airplane("Cessna", 1978, 5, 1700,0);
+        air1.setYear(1999);
+        air1.setLength(6);
+        air1.fillUp(5);
+        air1.fillUp(10);
+        air1.info();
+
 
     }
 }
