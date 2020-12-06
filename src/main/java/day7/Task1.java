52debc4caa87088c4772a32f2eaf249126c081c2
evgsavelov <savelov08@ya.ru>
2020-12-06 14:56:11 +0300

    complete day7 tasks

--- src/main/java/day7/Task1.java
+++ src/main/java/day7/Task1.java
@@ -2,6 +2,11 @@ package day7;
 
 public class Task1 {
     public static void main(String[] args) {
+        Airplane air1 = new Airplane("Cessna", 1978, 5, 1700);
+        Airplane air2 = new Airplane("Airbus", 1978, 523, 56200);
+
+        Airplane.compareAirplanes(air1, air2);
+
 
     }
 }
--- src/main/java/day7/Task2.java
+++ src/main/java/day7/Task2.java
@@ -1,7 +1,37 @@
 package day7;
 
+import java.util.Random;
+
 public class Task2 {
     public static void main(String[] args) {
+        Random rand = new Random();
+           int randValue = rand.nextInt(10) + 90;
+        Player pl1 = new Player(randValue);
+        Player pl2 = new Player(randValue);
+        Player pl3 = new Player(randValue);
+        Player pl4 = new Player(randValue);
+        //Player pl5 = new Player(randValue);
+        //Player pl6 = new Player(randValue);
+
+        pl1.info();
+        pl1.run();
+        pl1.run();
+        pl1.run();
+        pl1.run();
+        pl1.run();
+        pl1.run();
+        pl1.run();
+        pl1.run();
+        pl1.run();
+        pl1.run();
+        pl1.run();
+        pl1.run();
+        pl1.run();
+        pl1.run();
+        pl1.run();
+        pl1.run();
+        pl1.run();
+        pl1.info();
 
     }
 }
